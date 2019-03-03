0760390b2ab0482934f853d2b1676d086fe601e1
Michael Ruigrok <ruigrok.michael@gmail.com>
2019-03-03 23:58:25 +1000

    Add some js interoperability stuff to clojure so I have a decent picture next time

--- clojurescript/src/onion_room/core.cljs
+++ clojurescript/src/onion_room/core.cljs
@@ -1,4 +1,25 @@
 #!/usr/bin/clojure
-(ns onion-room.core)
+(ns onion-room.core
+	(:require [clojure.spec.alpha :as s])
+	)
 
+; For use in GAME object, if I use it
+(s/def ::player map?)
+(s/def ::gold boolean?)
+
+(def GAME
+	{::player {
+			   gold false
+			   }
+	 }
+	)
+
+(def input (.getElementById js/document "input"))
+(def output (.getElementById js/document "output"))
+
+(defn say [text]
+	(.insertAdjacentHTML output "beforeend" text)
+)
+
+(say "Welcome to your cool new adventure!")
 (println "Welcome to your cool new adventure!")
